9a159b20ed3d7c8f323aadfacbda102012e7c730
Victor Hernandez <victorhe92@gmail.com>
2026-08-07 22:51:33 -0400

    Add Web design and configure font and colors tailwind theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import { Button } from "@/components/ui/button";
 
 function App() {
   return (
-    <div className="h-screen bg-slate-900">
-      <h1 className="font-bold text-white underline">Hola mundo!</h1>
+    <div className="h-screen bg-white">
+      <h1 className="font-poppins font-normal">Hola mundo!</h1>
+      <h1 className="font-poppins font-semibold">Hola mundo!</h1>
+      <h1 className="font-poppins font-bold">Hola mundo!</h1>
+      <h1 className="font-poppins font-extrabold">Hola mundo2!</h1>
       <Button>hola</Button>
     </div>
   );
